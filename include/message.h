a8a9c466d3d0d2a52ca175cce8809737975a695c
Cyconi  <cyconiexo@gmail.com>
2025-02-06 10:19:21 -0800

    adjusted main so it passes compile flags

--- include/message.h
+++ include/message.h
@@ -4,46 +4,50 @@
 #include <stdint.h>
 
 #ifndef MESSAGE_H
-#define MESSAGE_H
+    #define MESSAGE_H
 
-struct Message {
-    uint8_t packet_type;
-    uint8_t protocol_version;
+struct Message
+{
+    uint8_t  packet_type;
+    uint8_t  protocol_version;
     uint16_t sender_id;
     uint16_t payload_length;
 };
 
-struct ACC_Login {
+struct ACC_Login
+{
     uint32_t username;
     uint32_t password;
 };
 
-struct ACC_Create {
+struct ACC_Create
+{
     uint32_t username;
     uint32_t password;
 };
 
-typedef enum {
-    BOOLEAN = 1,
-    INTEGER = 2,
-    NULL_VAL = 5,
-    ENUMERATED = 10,
-    UTF8STRING = 12,
-    SEQUENCE = 16,
+typedef enum
+{
+    BOOLEAN         = 1,
+    INTEGER         = 2,
+    NULL_VAL        = 5,
+    ENUMERATED      = 10,
+    UTF8STRING      = 12,
+    SEQUENCE        = 16,
     PRINTABLESTRING = 19,
-    UTCTIME = 23,
+    UTCTIME         = 23,
     GENERALIZEDTIME = 24,
 } Tag;
 
-typedef enum {
-    SYS_SUCCESS = 0,
-    SYS_ERROR = 1,
-    ACC_LOGIN = 10,
+typedef enum
+{
+    SYS_SUCCESS       = 0,
+    SYS_ERROR         = 1,
+    ACC_LOGIN         = 10,
     ACC_LOGIN_SUCCESS = 11,
-    ACC_LOGOUT = 12,
-    ACC_CREATE = 13,
-    ACC_EDIT = 14
+    ACC_LOGOUT        = 12,
+    ACC_CREATE        = 13,
+    ACC_EDIT          = 14
 } PacketType;
 
-
-#endif //MESSAGE_H
+#endif    // MESSAGE_H
--- src/client.c
+++ src/client.c
@@ -16,36 +16,13 @@
 
 #define PORT 8080
 
-#define P_BUFMAX 65541
+// #define P_BUFMAX 65541
 
-void send_acc_login(void);
-void send_acc_create(void);
-int  setup_socket(int *sockfd, struct sockaddr_in *serveraddr);
+// void send_acc_login(void);
+// void send_acc_create(void);
+// int  setup_socket(int *sockfd, struct sockaddr_in *serveraddr);
 
-int main(void)
-{
-    int                sockfd;
-    struct sockaddr_in serveraddr;
-    struct account     client;
-
-    if(setup_socket(&sockfd, &serveraddr) != 0)
-    {
-        printf("Could not establish connection. Exiting...\n");
-        return 1;
-    }
-
-    printf("Enter username: ");
-    scanf("%49s", client.username);
-
-    client.password = getpass("Enter password: ");
-
-    printf("\nUsername: %s\n", client.username);
-    printf("Password: %s\n", client.password);
-
-    close(sockfd);
-}
-
-int setup_socket(int *sockfd, struct sockaddr_in *serveraddr)
+static int setup_socket(int *sockfd, struct sockaddr_in *serveraddr)
 {
     *sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if(*sockfd == -1)
@@ -71,27 +48,51 @@ int setup_socket(int *sockfd, struct sockaddr_in *serveraddr)
     return 0;
 }
 
-void send_acc_login(const int *sockfd,struct account *client)
+static void send_acc_login(const int *sockfd, struct account *client)
 {
-    struct Message msg;
+    struct Message   msg;
     struct ACC_Login lgn;
 
-    lgn.username = client->username;
-    lgn.password = *client->password;
-
-    msg.packet_type = ACC_LOGIN;
-    msg.protocol_version = 1;
-    msg.sender_id = client->uid;
-    msg.payload_length = sizeof(lgn);
-
     uint8_t buffer[sizeof(msg) + sizeof(lgn)];
+
+    lgn.username = (uint32_t)*client->username;
+    lgn.password = (uint32_t)*client->password;
+
+    msg.packet_type      = ACC_LOGIN;
+    msg.protocol_version = 1;
+    msg.sender_id        = (uint16_t)client->uid;
+    msg.payload_length   = sizeof(lgn);
+
     memcpy(buffer, &msg, sizeof(msg));
     memcpy(buffer + sizeof(msg), &lgn, sizeof(lgn));
 
     send(*sockfd, buffer, sizeof(buffer), 0);
-
 }
 
-void send_acc_create(void)
+// static void send_acc_create(void) {}
+
+int main(void)
 {
+    int                sockfd;
+    struct sockaddr_in serveraddr;
+    struct account     client;
+
+    if(setup_socket(&sockfd, &serveraddr) != 0)
+    {
+        printf("Could not establish connection. Exiting...\n");
+        return 1;
+    }
+
+    printf("Enter username: ");
+    scanf("%49s", client.username);
+
+    client.password = getpass("Enter password: ");
+    client.uid      = 1;    // temp just so it builds
+
+    printf("\nUsername: %s\n", client.username);
+    printf("Password: %s\n", client.password);
+
+    send_acc_login(&sockfd, &client);
+
+    close(sockfd);
 }
